24108b44d720374d449e383bdbb8ea37aa819423
Aleksandr Grigorii <domosedov.dev@gmail.com>
2022-05-24 14:33:36 +0300

    add pathname

--- src/shared/router/model.ts
+++ src/shared/router/model.ts
@@ -1,16 +1,35 @@
+import { createStore, sample } from 'effector'
 import { createGate } from 'effector-react'
 import type { NavigateFunction, Location, Navigation } from 'react-router-dom'
+import { isBrowser } from '../env'
+import type { Nullable } from '../types/utils'
+import { isNotNull } from '../utils/is_not_null'
 
 type RouterGateProps = {
-  navigate?: NavigateFunction
-  location?: Location
-  navigation?: Navigation
+  location: Nullable<Location>
+  navigate: Nullable<NavigateFunction>
+  navigation: Nullable<Navigation>
 }
 
 export const RouterGate = createGate<RouterGateProps>({
+  defaultState: {
+    location: null,
+    navigate: null,
+    navigation: null,
+  },
   name: 'RouterGate',
 })
 
-export const $navigate = RouterGate.state.map(state => state.navigate)
+export const $pathname = createStore(isBrowser ? window.location.pathname : null)
 export const $location = RouterGate.state.map(state => state.location)
+export const $navigate = RouterGate.state.map(state => state.navigate)
 export const $navigation = RouterGate.state.map(state => state.navigation)
+
+sample({
+  clock: $location,
+  source: $pathname,
+  filter: (currentPathname, location) =>
+    isNotNull(location) && currentPathname !== location.pathname,
+  fn: (_, location) => location?.pathname as string,
+  target: $pathname,
+})
